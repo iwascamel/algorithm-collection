225473c41f206337de9c97a33ea92414f2793c37
ims-idlenara <gudwnsrh@i-nara.co.kr>
2022-11-17 00:23:14 +0900

    hmm...

--- src/문제집/leetcode/zig_zag_conversion/Main.java
+++ src/문제집/leetcode/zig_zag_conversion/Main.java
@@ -1,7 +1,9 @@
 package 문제집.leetcode.zig_zag_conversion;
 
+// https://velog.io/@redgem92/LeetCodeJava-Median-of-Two-Sorted-Arrays-uofx69x6
+// 해당 풀이 참조
 public class Main {
     public static void main(String[] args) {
-               
+        
     }
 }
